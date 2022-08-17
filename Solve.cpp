07b8cb070626663bc0c81524f48de3fe09b76cf4
d-r-tikhonov <d.r.tikhonov@yandex.ru>
2022-08-17 19:23:35 +0300

    Fix 1.0
    - нашёл значения, при которых программа ложится, пофиксил это, добавив частные случаи
    - code style++

--- Solve.cpp
+++ Solve.cpp
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <TXlib.h>
+#include "functions.h"
 
 int main(void){
 
-	double seniorCoeff, averageCoeff, freeTerm, discriminant; /* переменные для хранения коэффицентов уравнения */
+	double seniorCoeff = 0.0; /* переменная для старшего коэффициента уравнения */
+	double averageCoeff = 0.0; /* переменная для среднего коэффициента уравнения */
+	double freeTerm = 0.0; /* переменная для свободного члена уравнения */
 
-	seniorCoeff = 0.0;
-	averageCoeff = 0.0;
-	freeTerm = 0.0;
-	discriminant = 0.0;
+	printf ("Введите старший коэффицент: ");
+	scanf ("%lf", &seniorCoeff);
+	printf ("Введите средний коэффицент: ");
+	scanf ("%lf", &averageCoeff);
+	printf ("Введите свободный член: ");
+	scanf ("%lf", &freeTerm);
 
-	printf("Введите старший коэффицент: \n");
-	scanf("%lf", &seniorCoeff);
-	printf("Введите средний коэффицент: \n");
-	scanf("%lf", &averageCoeff);
-	printf("Введите свободный член: \n");
-	scanf("%lf", &freeTerm);
-
-	discriminant = pow(averageCoeff,2) - 4*seniorCoeff*freeTerm;
-
-	if(seniorCoeff == 0){
-		printf("Ваше уравнение является линейным, у него 1 корень, x = %f\n", (-freeTerm) / averageCoeff);
-	} else{
-		if(discriminant < 0) printf("Уравнение не имеет вещественных корней, так как дискриминант Вашего уравнения меньше нуля!\n");
-		if (discriminant == 0) printf("Ваше уравнения имеет 1 решение, x = %f\n", (-averageCoeff)/(2*seniorCoeff));
-		if (discriminant > 0) printf("Ваше уравнение имеет 2 корня, равные x1 = %f и x2 = %f", ((-averageCoeff)+sqrt(discriminant))/(2*seniorCoeff),((-averageCoeff)-sqrt(discriminant))/(2*seniorCoeff));
+	if (seniorCoeff == 0)
+	{
+		if (averageCoeff != 0)
+			lineRoot(averageCoeff, freeTerm);
+		else if (freeTerm == 0)
+			printf ("Ваше уравнение является линейным и имеет бесконечно много решений.");
+		else
+			printf("Ваше уравнение является линейным и не имеет решений.");
 	}
+
+	if (seniorCoeff != 0){
+
+		if (discriminant(seniorCoeff, averageCoeff, freeTerm) < 0)
+			negativeD();
+
+		if (discriminant(seniorCoeff, averageCoeff, freeTerm) == 0)
+			zeroD(seniorCoeff, averageCoeff);
+
+		if (discriminant(seniorCoeff, averageCoeff, freeTerm) > 0)
+			twoRoots(seniorCoeff, averageCoeff, freeTerm);
+	}
+
 	return 0;
 }
