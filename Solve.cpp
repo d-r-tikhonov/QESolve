e7e855834567d101b1d26ad2ef41927c03615aac
d-r-tikhonov <d.r.tikhonov@yandex.ru>
2022-08-23 15:17:18 +0300

    Fix 1.1
    -переписал почти всю программу
    - улучшил масштабируемость
    - перенёс всё в один файл

--- Solve.cpp
+++ Solve.cpp
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <TXlib.h>
-#include "functions.h"
+
+short SolveQE(double a, double b, double c, double *x1, double *x2);
 
 int main(void){
 
-	double seniorCoeff = 0.0; /* переменная для старшего коэффициента уравнения */
-	double averageCoeff = 0.0; /* переменная для среднего коэффициента уравнения */
-	double freeTerm = 0.0; /* переменная для свободного члена уравнения */
+	double seniorCoeff = 0.0;
+	double averageCoeff = 0.0;
+	double freeTerm = 0.0;
+	double firstR = 0.0;
+	double secondR = 0.0;
+	short nRoots = 0;
 
 	printf ("Введите старший коэффицент: ");
 	scanf ("%lf", &seniorCoeff);
@@ -15,27 +19,61 @@ int main(void){
 	printf ("Введите свободный член: ");
 	scanf ("%lf", &freeTerm);
 
-	if (seniorCoeff == 0)
-	{
-		if (averageCoeff != 0)
-			lineRoot(averageCoeff, freeTerm);
-		else if (freeTerm == 0)
-			printf ("Ваше уравнение является линейным и имеет бесконечно много решений.");
-		else
-			printf("Ваше уравнение является линейным и не имеет решений.");
-	}
+	nRoots = SolveQE(seniorCoeff, averageCoeff, freeTerm, &firstR, &secondR);
 
-	if (seniorCoeff != 0){
-
-		if (discriminant(seniorCoeff, averageCoeff, freeTerm) < 0)
-			negativeD();
-
-		if (discriminant(seniorCoeff, averageCoeff, freeTerm) == 0)
-			zeroD(seniorCoeff, averageCoeff);
-
-		if (discriminant(seniorCoeff, averageCoeff, freeTerm) > 0)
-			twoRoots(seniorCoeff, averageCoeff, freeTerm);
+	switch(nRoots){
+		case 0:
+            printf("Ваше уравнение не имеет корней.");
+           	break;
+		case 1:
+			printf("Ваше уравнение имеет 1 корень: %.2lf.", firstR);
+			break;
+		case 2:
+			printf("Ваше уравнение - квадратное и имеет 2 корня: x1 = %.2lf и x2 = %.2lf.", firstR, secondR);
+			break;
+		default:
+			printf("Ваше уравнение имеет бесконечно много корней.");
+			break;
 	}
 
 	return 0;
+}
+
+short SolveQE(double a, double b, double c, double *firstR, double *secondR){
+
+	double discriminant = 0.0;
+
+	if (a == 0)
+	{
+		if (b != 0)
+		{
+			*firstR=*secondR=(-c)/(b);
+			return 1;
+        }
+		else if (c == 0)
+			return -1;
+		else
+			return 0;
+	}
+
+	discriminant = b*b - 4*a*c;
+
+	if (a != 0){
+
+		if (discriminant < 0)
+			return 0;
+
+		if (discriminant == 0)
+		{
+			*firstR = *secondR = (-b)/(2*a);
+			return 1;
+        }
+
+		if (discriminant > 0)
+		{
+			*firstR = ((-b + sqrt(discriminant))/(2*a));
+			*secondR = ((-b - sqrt(discriminant))/(2*a));
+			return 2;
+        }
+	}
 }
